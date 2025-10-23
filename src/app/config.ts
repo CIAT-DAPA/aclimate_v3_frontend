fd3c7da765ba462c1771c2642339d881c3b2f168
santiagoC <s.calderon@cgiar.org>
2025-10-23 12:08:51 -0500

    Update environment variable prefixes in .env.example and config.ts

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,2 +1,2 @@
-export const API_URL = process.env.ACLIMATE_API_URL || 'http://127.0.0.1:8000';
-export const COUNTRY_ID = process.env.ACLIMATE_COUNTRY_ID || '1';
+export const API_URL = process.env.NEXT_PUBLIC_ACLIMATE_API_URL || 'http://127.0.0.1:8000';
+export const COUNTRY_ID = process.env.NEXT_PUBLIC_ACLIMATE_COUNTRY_ID || '1';
