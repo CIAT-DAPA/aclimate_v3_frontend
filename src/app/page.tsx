1500821b6ad4eec6b184be21ef259a104313d27a
santiagoC <s.calderon@cgiar.org>
2025-12-03 09:42:34 -0500

    Remove scroll indicator from Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,25 +43,6 @@ export default function Home() {
             </div>
           </div>
         </div>
-
-        {/* Indicador de scroll */}
-        <div className="absolute bottom-18 left-1/2 transform -translate-x-1/2 z-10">
-          <div className="animate-bounce">
-            <svg
-              className="w-6 h-6 text-amber-50"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M19 14l-7 7m0 0l-7-7m7 7V3"
-              />
-            </svg>
-          </div>
-        </div>
       </section>
 
     </div>
